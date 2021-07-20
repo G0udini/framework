63bc6926c4953389503923b88db2d169774b19f6
G0udini <ruslan.shabunin2010yandex.ru>
2021-07-20 23:20:50 +0300

    change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2ficzeamon%crw7(ytr_e1k#rbh2)xeks(+9y&yi#_!cwbq4d*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thawing-journey-37743.herokuapp.com']
 
 
 # Application definition
